9a71e4cce3a18cc79607cb5c9a19fd788677aeb6
yibaben <yibaben@gmail.com>
2023-04-06 18:31:21 +0100

    Employee DB SetUp and Jpa Entity

---
+++ Decagon-Employee-Service/src/main/java/com/decagon/DecagonEmployeeService/entity/Employee.java
@@ -0,0 +1,28 @@
+package com.decagon.DecagonEmployeeService.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "employees")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+}
---
+++ Decagon-Employee-Service/src/main/java/com/decagon/DecagonEmployeeService/repository/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.decagon.DecagonEmployeeService.repository;
+
+import com.decagon.DecagonEmployeeService.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}

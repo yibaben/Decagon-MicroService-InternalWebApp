f6a125cf278658622f8687c763597fa60f975a55
yibaben <yibaben@gmail.com>
2023-04-07 16:48:52 +0100

    Update Save and Get Employment REST API With ModelMapper Library

--- Decagon-Employee-Service/src/main/java/com/decagon/DecagonEmployeeService/DecagonEmployeeServiceApplication.java
+++ Decagon-Employee-Service/src/main/java/com/decagon/DecagonEmployeeService/DecagonEmployeeServiceApplication.java
@@ -1,13 +1,20 @@
 package com.decagon.DecagonEmployeeService;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DecagonEmployeeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DecagonEmployeeServiceApplication.class, args);
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
 	}
 
 }
--- Decagon-Employee-Service/src/main/java/com/decagon/DecagonEmployeeService/services/EmployeeServiceImpl.java
+++ Decagon-Employee-Service/src/main/java/com/decagon/DecagonEmployeeService/services/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import com.decagon.DecagonEmployeeService.dto.EmployeeDto;
 import com.decagon.DecagonEmployeeService.entity.Employee;
 import com.decagon.DecagonEmployeeService.repository.EmployeeRepository;
 import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
@@ -11,28 +12,36 @@ import org.springframework.stereotype.Service;
 public class EmployeeServiceImpl implements EmployeeService{
 
     private EmployeeRepository employeeRepository;
+    private ModelMapper modelMapper;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
         // Convert EmployeeDto to Jpa Entity and save into Database
-        Employee newEmployee = new Employee(
-                employeeDto.getId(),
-                employeeDto.getFirstName(),
-                employeeDto.getLastName(),
-                employeeDto.getEmail(),
-                employeeDto.getPassword()
-        );
+//        Employee newEmployee = new Employee(
+//                employeeDto.getId(),
+//                employeeDto.getFirstName(),
+//                employeeDto.getLastName(),
+//                employeeDto.getEmail(),
+//                employeeDto.getPassword()
+//        );
+
+        // Convert EmployeeDto to Jpa Entity and save into Database
+        // Using ModelMapper
+        Employee newEmployee = modelMapper.map(employeeDto, Employee.class);
         Employee savedEmployee = employeeRepository.save(newEmployee);
 
         // Convert SavedEmployee Jpa Entity in the DB to EmployeeDto for Client Request
-        EmployeeDto employee = new EmployeeDto(
-                savedEmployee.getId(),
-                savedEmployee.getFirstName(),
-                savedEmployee.getLastName(),
-                savedEmployee.getEmail(),
-                savedEmployee.getPassword()
-        );
-        return employee;
+//        EmployeeDto employee = new EmployeeDto(
+//                savedEmployee.getId(),
+//                savedEmployee.getFirstName(),
+//                savedEmployee.getLastName(),
+//                savedEmployee.getEmail(),
+//                savedEmployee.getPassword()
+//        );
+
+        // Convert SavedEmployee Jpa Entity in the DB to EmployeeDto for Client Request
+        // Using ModelMapper
+        return modelMapper.map(savedEmployee, EmployeeDto.class);
     }
 
     @Override
@@ -40,13 +49,15 @@ public class EmployeeServiceImpl implements EmployeeService{
         Employee employee = employeeRepository.findById(employeeId).get();
 
         // Convert Employee Jpa Entity to EmployeeDto
-        EmployeeDto employeeDto = new EmployeeDto(
-                employee.getId(),
-                employee.getFirstName(),
-                employee.getLastName(),
-                employee.getEmail(),
-                employee.getPassword()
-        );
-        return employeeDto;
+//        EmployeeDto employeeDto = new EmployeeDto(
+//                employee.getId(),
+//                employee.getFirstName(),
+//                employee.getLastName(),
+//                employee.getEmail(),
+//                employee.getPassword()
+//        );
+
+        // Convert Employee Jpa Entity to EmployeeDto Using ModelMapper
+        return modelMapper.map(employee, EmployeeDto.class);
     }
 }

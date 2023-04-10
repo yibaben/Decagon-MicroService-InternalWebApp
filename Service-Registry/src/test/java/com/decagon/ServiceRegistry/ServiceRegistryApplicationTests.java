47d4430719c3f235a8c56f40e6252a01aa6b3b6e
yibaben <yibaben@gmail.com>
2023-04-10 14:49:31 +0100

    Added Service Registry Project Module

---
+++ Service-Registry/src/test/java/com/decagon/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decagon.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

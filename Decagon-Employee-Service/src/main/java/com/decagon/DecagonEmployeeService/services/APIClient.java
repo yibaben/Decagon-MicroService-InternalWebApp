123243d14d81c41f35b4921b99a698378f7ea0c0
yibaben <yibaben@gmail.com>
2023-04-10 22:42:27 +0100

    Added Api-Gateway to our Project and Reg it in Eureka Server

--- Decagon-Employee-Service/src/main/java/com/decagon/DecagonEmployeeService/services/APIClient.java
+++ Decagon-Employee-Service/src/main/java/com/decagon/DecagonEmployeeService/services/APIClient.java
@@ -5,7 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(url = "http://localhost:8080", value = "DEPARTMENT-SERVICE")
+// With OpenFeign and Eureka Service Registry; Spring Cloud will Auto Implement
+// LoadBalancing to available Server Instances of Department-Service Whenever
+// a REST API Call is made.
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("api/department/get/{code}")
     DepartmentDto getDepartment(@PathVariable("code") String departmentCode);

d75ed4687d9f747dadce9c6d9bfe0ae9054557c2
yibaben <yibaben@gmail.com>
2023-04-12 15:26:11 +0100

    Added Spring-Cloud Bus to Dept/Emp Services

---
+++ Decagon-Employee-Service/src/main/java/com/decagon/DecagonEmployeeService/controller/MessageController.java
@@ -0,0 +1,20 @@
+package com.decagon.DecagonEmployeeService.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class MessageController {
+
+    // Read the values from properties file
+    @Value("${spring.boot.message}")
+    private String message;
+
+    @GetMapping("message")
+    public String getMessage(){
+        return message;
+    }
+}

dfdd4fcd04549dfa3736bd513c10072a4937694f
yibaben <yibaben@gmail.com>
2023-04-13 14:16:02 +0100

    Added Organisation Service

---
+++ Decagon-Organisation-Service/src/main/java/com/decagon/DecagonOrganisationService/DecagonOrganisationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.decagon.DecagonOrganisationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DecagonOrganisationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DecagonOrganisationServiceApplication.class, args);
+	}
+
+}

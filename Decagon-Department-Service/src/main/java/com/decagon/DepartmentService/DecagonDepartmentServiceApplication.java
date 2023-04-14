8c71a0c81b69bbbf0246a2a6831d56fe4f7742d5
yibaben <yibaben@gmail.com>
2023-04-14 22:37:29 +0100

    Define General API Information using Annotations

--- Decagon-Department-Service/src/main/java/com/decagon/DepartmentService/DecagonDepartmentServiceApplication.java
+++ Decagon-Department-Service/src/main/java/com/decagon/DepartmentService/DecagonDepartmentServiceApplication.java
@@ -1,10 +1,35 @@
 package com.decagon.DepartmentService;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Department-Service REST APIs",
+				description = "Department Service REST APIs Documentation",
+				version = "v1.0.0",
+				contact = @Contact(
+						name = "Bennett",
+						email = "yibaben@decagon.com",
+						url = "https://www.decagonhq.com.it"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.decagonhq.com.it"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Department Service Doc",
+				url = "https://www.decagonhq.com.it"
+		)
+)
 @SpringBootApplication
 public class DecagonDepartmentServiceApplication {
 

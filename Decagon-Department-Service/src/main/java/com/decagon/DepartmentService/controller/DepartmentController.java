55650516810a6b5581e9de38e221d94d1fcdb63c
yibaben <yibaben@gmail.com>
2023-04-14 22:59:30 +0100

    Customize Department-Service Swagger API Doc

--- Decagon-Department-Service/src/main/java/com/decagon/DepartmentService/controller/DepartmentController.java
+++ Decagon-Department-Service/src/main/java/com/decagon/DepartmentService/controller/DepartmentController.java
@@ -3,12 +3,19 @@ package com.decagon.DepartmentService.controller;
 import com.decagon.DepartmentService.dto.DepartmentDto;
 import com.decagon.DepartmentService.repository.DepartmentRepository;
 import com.decagon.DepartmentService.services.DepartmentService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@Tag(
+        name = "Department-Service - DepartmentController",
+        description = "Exposes REST APIs for Department-Service"
+)
 @RestController
 @AllArgsConstructor
 @RequestMapping("api/department")
@@ -17,6 +24,11 @@ public class DepartmentController {
     private DepartmentService departmentService;
 
     // Build Save Department REST API
+    @Operation(
+            summary = "Save new Department REST API",
+            description = "This REST API is used to save new Department object in a Database"
+    )
+    @ApiResponse(responseCode = "201", description = "HTTP Status 201 CREATED")
     @PostMapping("save")
     public ResponseEntity<DepartmentDto> savedDepartments(@RequestBody DepartmentDto departmentDto){
         DepartmentDto newDepartment = departmentService.saveDepartment(departmentDto);
@@ -24,6 +36,11 @@ public class DepartmentController {
     }
 
     // Build Get Department REST API
+    @Operation(
+            summary = "Get Department REST API",
+            description = "This REST API is used to Get or Retrieve Department object from the Database"
+    )
+    @ApiResponse(responseCode = "200", description = "HTTP Status 200 SUCCESS")
     @GetMapping("get/{code}")
     public ResponseEntity<DepartmentDto> getDepartment(@PathVariable("code") String departmentCode){
         DepartmentDto departmentDto = departmentService.getDepartmentByCode(departmentCode);
